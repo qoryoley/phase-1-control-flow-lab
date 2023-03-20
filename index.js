a0929d8811ff19b08e29cb44fc1b7f47f339b1a1
Abdalla <qoryoley@gmail.com>
2023-03-20 08:28:19 +0300

    Completed Assigment Control Flow

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(someNumber) {
+
+  if (someNumber <= 400){
+          return "This one is on me!";
+  }
+  else if (someNumber > 2000 && someNumber < 2500){
+      return "I will gladly take your thirty bucks.";
+  }
+  else if(someNumber > 2500){
+      return "No can do.";
+  }
+}
+function ternaryCheckCity(city) {
+
+  if (city === "NYC") {
+      return "Ok, sounds good.";
+  }
+  else{
+      return "No go.";
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
+function switchOnCharmFromTip(tip) {
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+  if (tip === "generous") {
+      return "Thank you so much.";
+  } else if (tip === "not as generous") {
+      return "Thank you.";
+  } else {
+      return "Bye.";
+  }
 }
